ffa277c5a937774b774bfbd89c245a332b62c859
mertwlf <mertwlf@gmail.com>
2020-03-29 22:42:37 +0300

    Update motar_control_sub.py

--- scripts/motar_control_sub.py
+++ scripts/motar_control_sub.py
@@ -12,41 +12,57 @@ gpio.setup(11,gpio.OUT)
 gpio.setup(13,gpio.OUT)
 gpio.setup(15,gpio.OUT)
 
-def getctrlinst():
-    rospy.init_node('motar_control_sub')
-
-    rospy.Subscriber("cmd_vel", Twist)
-    
-    key=Twist()
-    
+def getctrlinst(data):
     x = 0
     th = 0
     count = 0
 
     while(1):
-        x = key.linear.x
-        th = key.angular.z
+        x = data.linear.x
+        th = data.angular.z
         count = 0
-        if x==1:
+        if x==1 and th==0:#Forward
+            #RightWheel
+            gpio.output(11,True) 
             gpio.output(16,False)
-            gpio.output(11,True)        		   
+            #Left Wheel
             gpio.output(13,True)    		      
             gpio.output(15,False)
-        elif x==-1:
-            gpio.output(16,True)
+        elif x==-1 and th==0:#BackWard
             gpio.output(11,False)
+            gpio.output(16,True)
             gpio.output(13,False)
             gpio.output(15,True)
-        elif th==1:
-            gpio.output(16,False)
+        elif th==1 and x==0:#Left
             gpio.output(11,True)
+            gpio.output(16,False)           
             gpio.output(13,False)
             gpio.output(15,True)
-        elif th==-1:
-            gpio.output(16,True)
+        elif th==-1 and x==0:#Right
             gpio.output(11,False)
+            gpio.output(16,True)
             gpio.output(13,True)
             gpio.output(15,False)
+        elif x==1 and th==1:#FrontLeft
+            gpio.output(11,True)
+            gpio.output(16,False)           
+            gpio.output(13,False)
+            gpio.output(15,False)
+        elif x==1 and th==-1:#FrontRight
+            gpio.output(11,False) 
+            gpio.output(16,False)
+            gpio.output(13,True)    		      
+            gpio.output(15,False)
+        elif x==-1 and th==1:#BackwardLeft
+            gpio.output(11,False)
+            gpio.output(16,True)
+            gpio.output(13,False)
+            gpio.output(15,False)
+        elif x==-1 and th==-1:#BakwardRight
+            gpio.output(11,False) 
+            gpio.output(16,False)
+            gpio.output(13,False)    		      
+            gpio.output(15,True)
         elif x==0 and th==0:
             gpio.output(16,False)
             gpio.output(11,False) 
@@ -62,4 +78,6 @@ def getctrlinst():
             break
              
 if __name__=="__main__":
-    getctrlinst()
+    rospy.init_node('motar_control_sub')
+    rospy.Subscriber("cmd_vel", Twist,getctrlinst)
+    
