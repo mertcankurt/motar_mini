30834dbd624557b0e1ceca0fddfb913fc591e011
mertwlf <mertwlf@gmail.com>
2020-07-02 23:48:23 +0300

    navigation

---
+++ scripts/sensor_data_listener.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+import sensor_msgs.msg
+import random
+import numpy as np
+from geometry_msgs.msg import Twist
+from itertools import *
+from operator import itemgetter
+
+LINX = 0.0 #Always forward linear velocity.
+PI = 3.14
+angz = 0
+
+
+def SonarFScan(data):
+    global LINX
+    global angz
+    try:
+        x = data.range
+        if(x<=0.3):
+            LINX = LINX-0.2
+
+        
+    except Exception as err:
+        print(err)
+
+def SonarLScan(data):
+    global angz
+    try:
+        x = data.range
+        
+        if(x<0.3):
+            angz= angz+0.2
+        else: angz=angz
+    except Exception as err:
+        print(err)
+
+def SonarRScan(data):
+    global angz
+    try:
+        x = data.range
+        if(x<0.3):
+            angz= angz-0.2
+        else: 
+            angz=angz
+    except Exception as err:
+        print(err)
+def VelScan(data):
+    global angz
+    global LINX
+    LINX = data.linear.x
+    angz = data.angular.z
+def main():
+    
+    rospy.init_node('listener', anonymous=True)
+
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    
+    rospy.Subscriber("/cmd_vel",Twist, VelScan)
+    rospy.Subscriber("/SF", sensor_msgs.msg.Range , SonarFScan)
+    rospy.Subscriber("/SL", sensor_msgs.msg.Range , SonarLScan)
+    rospy.Subscriber("/SR", sensor_msgs.msg.Range , SonarRScan)
+
+    rate = rospy.Rate(20) # 10hz
+
+    while not rospy.is_shutdown():
+        command = Twist()
+        command.linear.x = LINX
+        command.angular.z = angz
+        pub.publish(command)
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
--- scripts/waypoint_nav.py
+++ scripts/waypoint_nav.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python
-
-'''
-Copyright (c) 2015, Mark Silliman
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
-
-# TurtleBot must have minimal.launch & amcl_demo.launch
-# running prior to starting this script
-# For simulation: launch gazebo world & amcl_demo prior to run this script
-
 import rospy
+from std_msgs.msg import String
+import sensor_msgs.msg
+import random
+import numpy as np
+from geometry_msgs.msg import Twist
+from operator import itemgetter
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
 from trajectory_msgs.msg import *
 from visualization_msgs.msg import *
 from geometry_msgs.msg import Pose, Point, Quaternion
+from itertools import *
+
+
 
 class GoToPose():
     def __init__(self):
@@ -56,7 +45,7 @@ class GoToPose():
         self.move_base.send_goal(goal)
 
 	# Allow TurtleBot up to 60 seconds to complete task
-	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+	success = self.move_base.wait_for_result(rospy.Duration(90)) 
 
         state = self.move_base.get_state()
         result = False
@@ -79,37 +68,41 @@ class GoToPose():
 if __name__ == '__main__':
     try:
         rospy.init_node('nav_test', anonymous=False)
+
         navigator = GoToPose()
 
         # Customize the following values so they are appropriate for your location
-        position = {'x': -8.0, 'y' :1.5}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+        position = {'x': -4.3, 'y' :9.9}
+        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : -1.000}
+        
         
         tolerance = 0.5
 
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+        
         success = navigator.goto(position, quaternion)
 #        success = navigator.goto(position)
 
         #position = {'x': 2.5, 'y' :-2.5}
-        position = {'x': -8, 'y' :4}
+        position = {'x': -11, 'y' :-5}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+        
 
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
         success = navigator.goto(position, quaternion)
 
-        position = {'x': -12, 'y' :4}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
-
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-
-        position = {'x': -2.5, 'y' :1.0}
+        position = {'x': -4, 'y' :-5}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.520, 'r4' : 1.000}
+        
 
         rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
         success = navigator.goto(position, quaternion)
 
+        position = {'x': -11, 'y' :-9}
+        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.600, 'r4' : -0.750}
+
+        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
+        success = navigator.goto(position, quaternion)
 
         if success:
             rospy.loginfo("I'm here now... (%s, %s) pose", position['x'], position['y'])
@@ -117,6 +110,7 @@ if __name__ == '__main__':
             rospy.loginfo("The base failed to reach the desired pose")
 
         # Sleep to give the last log messages time to be sent
+        
         rospy.sleep(1)
 
     except rospy.ROSInterruptException:
